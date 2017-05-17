f2b68a3ad4dd6bfe1e84f1ff23e770f28cb250d6
sweswe-frobom <khine.swe.swe.aung@frobom.com>
2017-05-17 11:34:00 +0630

    first commit

---
+++ src/de/vogella/jsf/card/model/Card.java
@@ -0,0 +1,41 @@
+package de.vogella.jsf.card.model;
+
+import java.util.Random;
+
+public class Card {
+	private int left;
+	private int right;
+
+	public Card() {
+		Random random = new Random();
+		int i = 0;
+		int j = 0;
+		do {
+			i = random.nextInt(10);
+		} while (i <= 4);
+
+		do {
+			j = random.nextInt(100);
+		} while (j <= 20);
+
+		left = i;
+		right = j;
+	}
+
+	public int getLeft() {
+		return left;
+	}
+
+	public void setLeft(int left) {
+		this.left = left;
+	}
+
+	public int getRight() {
+		return right;
+	}
+
+	public void setRight(int right) {
+		this.right = right;
+	}
+
+}
